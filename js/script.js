13b3e7ddebbc622206d96f4b24ecae3806b05413
phamlequocphi-1609 <phamlequocphi16092001@gmail.com>
2025-03-23 15:19:21 +0700

    first project

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.getElementById("hamburger");
+    const navLinks = document.getElementById("nav-links");
+
+    hamburger.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
+    });
+});
+
+const slides = document.querySelectorAll(".slide");
+let currentIndex = 0;
+document.querySelector(".next-btn").addEventListener("click", () => {
+    slides[currentIndex].classList.remove("active");
+    currentIndex = (currentIndex + 1) % slides.length;
+    slides[currentIndex].classList.add("active");
+});
+
+document.querySelector(".prev-btn").addEventListener("click", () => {
+    slides[currentIndex].classList.remove("active");
+    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+    slides[currentIndex].classList.add("active");
+});
+
+
