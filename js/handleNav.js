2220dc7a0a724e50ba1e8ee833f48dfef7b923cd
phamlequocphi-1609 <phamlequocphi16092001@gmail.com>
2025-03-23 18:25:53 +0700

    up url

--- js/handleNav.js
+++ js/handleNav.js
@@ -1,5 +1,5 @@
 const currentPath = window.location.pathname;
-console.log(currentPath);
+
 const navLinks = document.querySelectorAll(".nav-links li a");
 
 navLinks.forEach((link) => {
@@ -8,3 +8,28 @@ navLinks.forEach((link) => {
     link.classList.add("active");
   }
 });
+
+if (currentPath.includes("menu.html")) {
+  const header = document.querySelector(".header");
+  const heroText = document.querySelector(".hero-text h1");
+  const ctaButtons = document.querySelector(".cta-buttons");
+
+  header.style.background =
+    "url('https://kenh14cdn.com/203336854389633024/2022/5/29/photo-1-1653786925158170035088.jpg') center/cover no-repeat";
+
+  if (heroText) {
+    heroText.textContent = "Khám phá thực đơn đặc sắc của chúng tôi!";
+  }
+  if (ctaButtons) {
+    ctaButtons.innerHTML = `
+        <a href="/confirm-order.html" class="yes" style="padding: 24px">
+          <p><span>Order</span></p>
+          <p>Now</p>
+        </a>
+        <a href="/index.html" class="no" style="padding: 24px">
+          <p><span>Back</span></p>
+          <p>Home</p>
+        </a>
+      `;
+  }
+}
